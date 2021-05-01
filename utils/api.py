eac79c26964ebc3451d51e34db89333d73d565d7
lucasblazzi <lucasblazzi@hotmail.com>
2021-05-01 13:55:45 -0300

    Update api class

--- utils/api.py
+++ utils/api.py
@@ -1,70 +1,54 @@
 import requests
-import json
 from config import TOKEN as token
+import json
 
 
-def _url(path, version):
-    return f"https://sandbox.iexapis.com/{version}/{path}"
+class API:
+    def __init__(self, obj):
+        self.endpoint = obj.get("endpoint")
+        self.symbol = obj.get("symbol")
+        self.sector = obj.get("sector")
+        self.period = obj.get("period")
+        self.limit = obj.get("limit")
+        self.is_mock = obj.get("is_mock", False)
 
+    def _endpoint(self):
+        endpoints = {
+            "symbols": "/ref-data/symbols",
+            "crypto_symbols": "/ref-data/crypto/symbols",
+            "sectors": "/ref-data/sectors",
+            "sectors_performance": "/stock/market/sector-performance",
+            "upcoming_ipos": "/stock/market/upcoming-ipos",
+            "company": f"/stock/{self.symbol}/company",
+            "historical": f"/stock/{self.symbol}/chart/{self.period}",
+            "dividends": f"/time-series/advanced_dividends/{self.symbol}?last={self.limit}",
+            "return_of_capital": f"/time-series/advanced_return_of_capital/{self.symbol}?last={self.limit}",
+            "collection": f"/stock/market/collection/sector?collectionName={self.sector}",
+            "news": f"/time-series/news/{self.symbol}?range={self.period}&limit={self.limit}"
+        }
+        return endpoints.get(self.endpoint)
 
-def _response_parser(response):
-    if response.status_code == 200:
-        return response.json()
-    elif response.status_code == 404:
-        print(f"Not Found - URL: {response.url}")
-    elif response.status_code == 403:
-        print(f"Api version error: {response.url}")
-    else:
-        print(f"An error occurred")
+    @staticmethod
+    def _url(path):
+        return f"https://sandbox.iexapis.com/stable{path}"
 
+    @staticmethod
+    def _response_parser(response):
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            print(f"Not Found - URL: {response.url}")
+        elif response.status_code == 403:
+            print(f"Api version error: {response.url}")
+        else:
+            print(f"An error occurred")
 
-def get_symbols():
-    return requests.get(_url("/ref-data/symbols", "stable"), params=token)
+    def mock(self):
+        r = self.get()
+        with open(f"D:\Documents\GitHub\stocker\mock\{self.endpoint}_mock.json", "w") as write_file:
+            json.dump(r, write_file)
 
-
-def get_crypto_symbols():
-    return requests.get(_url("/ref-data/crypto/symbols", "v1"), params=token)
-
-
-def get_sectors():
-    return requests.get(_url("/ref-data/sectors", "v1"), params=token)
-
-
-def get_sector_performance():
-    return requests.get(_url(f"/stock/market/sector-performance", "stable"), params=token)
-
-
-# doesnt work on sandbox
-def get_upcoming_ipo():
-    return requests.get(_url(f"/stock/market/upcoming-ipos", "stable"), params=token)
-
-
-def get_historical(symbol: str, period: str):
-    return requests.get(_url(f"/stock/{symbol}/chart/{period}", "v1"), params=token)
-
-
-def get_company(symbol: str):
-    return requests.get(_url(f"/stock/{symbol}/company", "stable"), params=token)
-
-
-# expensive
-def get_dividends(symbol: str, qtd: int):
-    return requests.get(_url(f"/time-series/advanced_dividends/{symbol}?last={qtd}", "stable"), params=token)
-
-
-# expensive
-def get_return_of_capital(symbol: str, qtd: int):
-    return requests.get(_url(f"/time-series/advanced_return_of_capital/{symbol}?last={qtd}", "stable"), params=token)
-
-
-def get_collection(sector: str):
-    return requests.get(_url(f"/stock/market/collection/sector?collectionName={sector}", "stable"), params=token)
-
-
-def get_news(symbol: str, period: str, limit: int):
-    return requests.get(_url(f"/time-series/news/{symbol}?range={period}&limit={limit}", "stable"), params=token)
-
-
-def write(m, r):
-    with open(f"D:\Documents\GitHub\stocker\mock\{m}_mock.json", "w") as write_file:
-        json.dump(r, write_file)
+    def get(self):
+        path = self._endpoint()
+        r = requests.get(self._url(path=path), params=token)
+        return self._response_parser(r)
