0f655a3b8b07e38092bd12cee9280ee2587197e4
lucasblazzi <lucasblazzi@hotmail.com>
2021-05-05 23:05:47 -0300

    Add basic auth

--- src/premium/app/__main__.py
+++ src/premium/app/__main__.py
@@ -1,8 +1,37 @@
 import streamlit as st
-import pandas as pd
 
 
-st.set_page_config(layout='wide')
-pd.set_option('display.max_colwidth', None)
+def is_authenticated(username, password):
+    return username == "admin" and password == "admin"
 
-side_bar = st.sidebar
+
+def generate_login_block():
+    block1 = st.empty()
+    block2 = st.empty()
+    return block1, block2
+
+
+def clean_blocks(blocks):
+    for block in blocks:
+        block.empty()
+
+
+def login(blocks):
+    username_block = blocks[0].text_input("Username", type="default")
+    pass_block = blocks[1].text_input("Username", type="password")
+    return username_block, pass_block
+
+
+def main():
+    st.header('Hello')
+    st.balloons()
+
+
+login_blocks = generate_login_block()
+username, password = login(login_blocks)
+
+if is_authenticated(username, password):
+    clean_blocks(login_blocks)
+    main()
+else:
+    st.info("Please provida valid credentials")
